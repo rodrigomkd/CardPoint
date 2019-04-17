3e3f09b247272c41c16e30b12b21451acb0bc731
Rodrigo Moncada <moncada@ibm.com>
2019-04-17 01:36:41 -0500

    Added logic to expire points for clients.

--- js/getPointsByClientId.php
+++ js/getPointsByClientId.php
@@ -2,7 +2,7 @@
 	$data = json_decode(file_get_contents("php://input"));
 	include('config.php');
 	$db = new DB();
-	$sql = "SELECT c.commerce_name, DATE_FORMAT(p.buy_date,'%d/%m/%Y') as buy_date, DATE_FORMAT(p.register_date,'%d/%m/%Y') as register_date, client.credential_number, ticket_number, quantity, points, percent from points p left outer join commerce c 
+	$sql = "SELECT c.commerce_name, DATE_FORMAT(p.buy_date,'%d/%m/%Y') as buy_date, DATE_FORMAT(p.register_date,'%d/%m/%Y') as register_date, client.credential_number, ticket_number, quantity, points, percent, valid, case valid when 1 then 'black' when 0 then 'red' end as style from points p left outer join commerce c 
 		on p.commerceid = c.commerceid
 		left outer join client client
 		on p.clientid = client.clientid
